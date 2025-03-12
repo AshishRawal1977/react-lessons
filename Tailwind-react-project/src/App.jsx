f080cb6cff611060833bb32c9c6b3ccf020e22d8
ashishrawal1977 <rawalashish1977@gmail.com>
2025-03-12 16:19:22 +0530

    Added 03_project and TailwindProject

--- Tailwind-react-project/src/App.jsx
+++ Tailwind-react-project/src/App.jsx
@@ -1,15 +1,62 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [color, setColor] = useState("pink");
 
   return (
-  <>
-    <h1 className="bg-acqu">Ashish Rawal</h1>
-  </>
+    <div
+      className={`w-screen h-screen transition-all`}
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 py-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-2xl rounded-xl bg-white px-4 py-3 ">
+          <button
+            onClick={() => setColor("red")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-2xl"
+            style={{ background: "red" }}
+          >
+            Red
+          </button>
+          <button
+            onClick={() => setColor("green")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-2xl"
+            style={{ background: "green" }}
+          >
+            Green
+          </button>
+          <button
+            onClick={() => setColor("orange")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-2xl"
+            style={{ background: "orange" }}
+          >
+            Orange
+          </button>
+          <button
+            onClick={() => setColor("olive")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-2xl"
+            style={{ background: "olive" }}
+          >
+            Olive
+          </button>
+          <button
+            onClick={() => setColor("blue")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-2xl"
+            style={{ background: "blue" }}
+          >
+            Blue
+          </button>
+          <button
+            onClick={() => setColor("black")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-2xl"
+            style={{ background: "black" }}
+          >
+            Black
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
